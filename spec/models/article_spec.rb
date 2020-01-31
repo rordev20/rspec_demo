97f15833ff907f1b0f20a61c6d3fa5ab134fa908
Rohit Patel <rohit.patel@infibeam.net>
2020-01-31 17:13:15 +0530

    check article creation

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	context "with 2 or more articles" do
+    it "orders them in reverse chronologically" do
+      article1 = Article.create! ({title: 'first article'})
+      article2 = Article.create! ({title: 'second article'})
+      expect(Article.all).to eq([article1, article2])
+    end
+  end
+
 end
